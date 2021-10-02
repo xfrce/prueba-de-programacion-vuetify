e6676daea2df1f8e0671a4030790542eca8318c7
xfrce <xfreelance@gmail.com>
2021-10-02 18:22:16 -0500

    Add files via upload

--- src/views/VehicleView.vue
+++ src/views/VehicleView.vue
@@ -11,25 +11,10 @@
           <v-card-title class="text-h5 grey lighten-2">
             vehiculo
           </v-card-title>
-
           <v-card-text>
             name
           </v-card-text>
-
-          <v-divider></v-divider>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-            >
-              Cerrar
-            </v-btn>
-          </v-card-actions>
         </v-card>
-
 
       </v-dialog>
     </div>
